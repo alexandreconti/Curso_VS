36febbabbf711768caeec9c5faadaa52402506db
Alexandre Arrieche De Conti <alexandreconti@gmail.com>
2019-04-22 16:45:51 +0200

    Complemento
    Teste de alteração no código.

--- 72_JaggedArray/72_JaggedArray/Program.cs
+++ 72_JaggedArray/72_JaggedArray/Program.cs
@@ -24,17 +24,17 @@ namespace _72_JaggedArray
             Console.WriteLine("Oi {0} te apresento {1}", x[1][1], x[0][1]);
             Console.WriteLine("Oi {0} te apresento {1}", x[2][0], x[1][1]);
 
-            /*for (int f = 0; f < x.Length; f++)
+            for (int f = 0; f < x.Length; f++)
             {
                 Console.WriteLine("\nElemento {0}", f);
-                for (int z = 0;z < x.Length; z++)
+                for (int z = 0; z < x[f].Length; z++)
                 {
                     
                     Console.WriteLine("{0}", x[f][z]);
                     
                 }
                 
-            }*/
+            }
 
             Console.Read();
 
